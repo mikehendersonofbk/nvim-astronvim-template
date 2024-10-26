c9698922f5f80de801ebbc55bc80f1fd3326a758
Michael Henderson <mike.w.henderson.88@gmail.com>
2024-10-25 20:26:27 -0400

    Configuring python golang and treesitter

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -38,13 +36,30 @@ return {
       -- end
     },
     -- enable servers that you already have installed without mason
-    servers = {
-      -- "pyright"
-    },
+    servers = {},
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      basedpyright = {
+        python = {
+          analysis = {
+            diagnosticMode = "openFilesOnly",
+          },
+        },
+      },
+      gopls = {
+        settings = {
+          go = {
+            usePlaceholders = true,
+            completeFunctionCalls = true,
+            analyses = {
+              unreachable = true,
+              unusedvariable = true,
+            },
+            hoverKind = "FullDocumentation",
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
